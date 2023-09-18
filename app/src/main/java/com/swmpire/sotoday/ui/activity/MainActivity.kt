a51f131e354e938315cd572d7b1f112eeb9ac827
ayopeepee <topdenmax@mail.ru>
2023-09-18 21:24:35 +0300

    Refactor bottom menu

--- app/src/main/java/com/swmpire/sotoday/ui/activity/MainActivity.kt
+++ app/src/main/java/com/swmpire/sotoday/ui/activity/MainActivity.kt
@@ -3,46 +3,31 @@ package com.swmpire.sotoday.ui.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.swmpire.sotoday.R
-import com.swmpire.sotoday.ui.fragment.TodayAllEventsFragment
-import com.swmpire.sotoday.ui.fragment.TodayEventFragment
 import com.swmpire.sotoday.viewmodel.EventViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomNav: BottomNavigationView
     private val sharedViewModel: EventViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottomNav = findViewById(R.id.bottom_menu)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_menu)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+                    as NavHostFragment
 
-        bottomNav.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.home -> {
-                    loadFragment(TodayEventFragment())
-                    true
-                }
-                R.id.list -> {
-                    loadFragment(TodayAllEventsFragment())
-                    true
-                }
+        val navController = navHostFragment.navController
+            bottomNavigationView.setupWithNavController(navController)
 
-                else -> {true}
-            }
-        }
     }
 
-    private fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.nav_host_fragment_container,fragment)
-        transaction.commit()
-    }
 
 }
